a45a1c65efa33caa14c6aece32ef5b639079fc25
Mateo Chumbita <mateo.chumbita@gmail.com>
2025-07-16 14:53:05 -0300

    Add changes to dist

--- dist/presentation/users/controller.d.ts
+++ dist/presentation/users/controller.d.ts
@@ -2,11 +2,7 @@ import { Request, Response } from "express";
 export declare class UserController {
     constructor();
     registerUser: (req: Request, res: Response) => Promise<void>;
-    postUser: (req: Request, res: Response) => Promise<void>;
-    getUser: (req: Request, res: Response) => Promise<void>;
     updateUser: (req: Request, res: Response) => Promise<void>;
-    getUsers: (req: Request, res: Response) => Promise<void>;
     getClerkUsers: (req: Request, res: Response) => Promise<void>;
     getClerkUser: (req: Request, res: Response) => Promise<void>;
-    deleteUser: (req: Request, res: Response) => Promise<void>;
 }
